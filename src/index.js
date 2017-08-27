ab39d540c8be8a6005198029bd9f1a8d9bb0b16b
stingy <1372997024@qq.com>
2017-08-27 15:28:21 +0800

    add css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(
-    <h1>Hi, world!</h1>,
+    <h1>Hello, world!</h1>,
     document.getElementById('root2')
 );
+ReactDOM.render(
+    <p>This is my first react app</p>,
+    document.getElementsByClassName('description')[0]
+)
 registerServiceWorker();
