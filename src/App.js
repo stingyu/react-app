5534ce37fed8ab00fbcca283a5fe23cb164591ef
stingy <1372997024@qq.com>
2017-08-29 15:35:37 +0800

    fix value add onChange

--- src/App.js
+++ src/App.js
@@ -22,19 +22,26 @@ class App extends Component {
         </li>
       )
     })
-    console.log(todos)
     return (
       <div className="App">
         <h1>ToDoList</h1>
         <div className = "inputWrapper">
           {/*注意value后面不要加引号*/}
-          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)}/>
+          <TodoInput content={this.state.newTodo} 
+                     onChange={this.changeTitle.bind(this)}
+                     onSubmit={this.addTodo.bind(this)}/>
         </div>
         <ol>
           {todos}
         </ol>
       </div>
     )
+  }
+  changeTitle (event) {
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
   }
   addTodo (event) {
     this.state.todoList.push({
